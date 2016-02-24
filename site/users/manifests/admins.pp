cb15548c5aba295b6ce73f6ac471363408acbf50
randyotg <randyotg@gmail.com>
2016-02-24 14:40:20 -0800

    more typos!!!!!!!

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,5 +1,5 @@
 class users::admins {
-  users::managed_users { 'jose': }
-  users::managed_users { 'alice': }
-  users::managed_users { 'chen': }
+  users::managed_user { 'jose': }
+  users::managed_user { 'alice': }
+  users::managed_user { 'chen': }
 }
